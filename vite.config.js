f1bd9dce26f059e9bbdad866b4d40284c7672508
adesopiann <adesopian884@gmail.com>
2024-07-22 08:38:04 +0700

    customize vercel.json

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        rollupOptions: {
-            output: {
-                entryFileNames: "assets/[name].js",
-                chunkFileNames: "assets/[name].js",
-                assetFileNames: "assets/[name].[ext]",
-            },
-        },
-    },
 });
